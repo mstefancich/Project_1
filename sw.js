834f98b8fa4baa982705c0e0b49b147a21553761
Marco Stefancich <mstefancich@gmail.com>
2022-11-01 18:16:15 +0100

    SW caching

--- sw.js
+++ sw.js
@@ -4,6 +4,15 @@ self.addEventListener("install", e => {
         caches.open("static").then(cache => {
             return cache.addAll(["./", "./src/master.css", "./src/images/logo192.png"]);
         })
-    ) ;
+    );
     console.log("Resources in cache. Done");
+});
+
+self.addEventListener("fetch", e => {
+    console.log('intercepting fecth request for:', e.request.url );
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request) ;
+        })
+    )
 }) ;
