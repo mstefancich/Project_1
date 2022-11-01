988554f75c0d1cefdaa2b7c6921cd226d4dac1aa
Marco Stefancich <mstefancich@gmail.com>
2022-11-01 15:53:45 +0100

    Service Woker test

--- src/script.js
+++ src/script.js
@@ -1,3 +1,15 @@
+if ("serviceWorker" in navigator) {
+    console.log("Service Worker supported");
+    navigator.serviceWorker.register("sw.js").then(ServiceWorkerRegistration => {
+        console.log("SW Registered");
+        console.log(ServiceWorkerRegistration);
+    }).catch(error => {
+        console.log("SW Registration failed");
+        console.log(error);
+    })
+}
+
+
 function runCode() {
     alert("Hello world");
 }
